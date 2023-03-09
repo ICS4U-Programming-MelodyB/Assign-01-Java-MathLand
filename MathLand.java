f3e1fb3d296cbb8594d162506c7d7f0853077374
Melody Berhane <melody.berhane@stu.ocsb.ca>
2023-03-09 00:27:45 +0000

    Done Everything!

--- MathLand.java
+++ MathLand.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 /**
-* The Program calculates the volume of a sphere in Java
-* and check input for non numerical inputs.
+* The Program asks the they want to do and if they choice 1
+* then they have the chance to find the area of a
+* triangle, trapezoid or pentagon
+* and if they choice 2 then they find the average of three numbers.
+*
 *
 * @author  Melody Berhane
 * @version 1.0
-* @since   2023-2-10
+* @since   2023-3-08
 */
 public final class MathLand {
 
@@ -31,7 +34,8 @@ public final class MathLand {
         // Declare Constants
         final String SHAPE_ONE = "triangle";
         final String SHAPE_TWO = "trapezoid";
-        final String SHAPE_THREE = "Pentagon";
+        final String SHAPE_THREE = "pentagon";
+
         // Declare variables
         String userString = " ";
         String shapeString = " ";
@@ -40,7 +44,8 @@ public final class MathLand {
         String trapBaseString = " ";
         String trapHeightString = " ";
         String trapSideString = " ";
-        String quesAns = " ";
+        String penSideString = " ";
+        final String quesAns = " ";
         String userNum1Sr = " ";
         String userNum2Sr = " ";
         String userNum3Sr = " ";
@@ -50,55 +55,74 @@ public final class MathLand {
         float trapBaseFloat = 0;
         float trapHeightFloat = 0;
         float trapSideFloat = 0;
+        double penSideDouble = 0;
         float triArea = 0;
         float trapArea = 0;
         float userNum1Float = 0;
         float userNum2Float = 0;
         float userNum3Float = 0;
         float average = 0;
+        double penArea = 0;
 
-        // Welcome the user
+        // Welcome the user and asks them what they would like to do.
         System.out.println("Hello! Welcome to Melody's MATH WONDERLAND!");
-        System.out.print("Please enter 1 to find the area of a triangle, trapezoid or Pentagon ");
+        System.out.print("Please enter 1 to find the area of");
+        System.ou.print(" a triangle, trapezoid or Pentagon ");
         System.out.print("and 2 to find the average: ");
         // Create a new scanner constant
         final Scanner scanner = new Scanner(System.in);
 
+        // Runs the program once.
         do {
-            System.out.print("Please enter 1 to find the area of a triangle, trapezoid or Pentagon ");
+            System.out.print("Please enter 1 to find the area of");
+            System.ou.print(" a triangle, trapezoid or Pentagon ");
             System.out.print("and 2 to find the average: ");
-            //Get next line as a string
+            // Get next line as a string
             userString = scanner.nextLine();
 
             try {
                 // Convert the string to a double
                 userInt = Integer.parseInt(userString);
 
-                // If radius is negative, tell the user
+                // If user inputs 1 they want to find the area of a shape
                 if (userInt == 1) {
+                    // Asks user what shape they would like
                     System.out.print("Would you like to find the area of a");
                     System.out.print(" Triangle, Trapezoid or Pentagon: ");
                     shapeString = scanner.nextLine();
-                    if(shapeString.equals(SHAPE_ONE)){
+                    // Finds the area of a triangle.
+                    if (shapeString.equals(SHAPE_ONE)) {
+                        // Gets the base as string
                         System.out.print("What is the base: ");
                         triBaseString = scanner.nextLine();
                         try {
+                            // Converts base to float
                             triBaseFloat = Float.parseFloat(triBaseString);
+                            // Gets the height as a string
                             System.out.print("What is the height: ");
                             triHeightString = scanner.nextLine();
                             try {
-                                triHeightFloat = Float.parseFloat(triHeightString);
+                                // Converts height to float
+                                triHeightFloat = Float.parseFloat(
+                                    triHeightString);
+                            // If the input is not a float display error
                             } catch (NumberFormatException error) {
-                                System.out.println("Please enter a valid integer "
+                                System.out.print("Please enter a valid ");
+                                System.out.println("number "
                                     + error.getMessage());
                             }
+                            // Calculate area of triangle
                             triArea = (triBaseFloat * triHeightFloat) / 2;
-                            System.out.println("The area of the Triangle is " + triArea + " (cm^2)");
+                            // Display the area
+                            System.out.print("The area of the ");
+                            System.out.println("Triangle is "
+                                + triArea + " (cm^2)");
+                        // If the input is not a float display error
                         } catch (NumberFormatException error) {
-                            System.out.println("Please enter a valid integer "
+                            System.out.println("Please enter a valid number "
                                 + error.getMessage());
                         }
-                    }else if(shapeString.equals(SHAPE_TWO)){
+                    } else if (shapeString.equals(SHAPE_TWO)) {
                         System.out.print("What is the side length: ");
                         trapSideString = scanner.nextLine();
                         try {
@@ -106,103 +130,128 @@ public final class MathLand {
                             System.out.print("What is the base: ");
                             trapBaseString = scanner.nextLine();
                             try {
-                                trapBaseFloat = Float.parseFloat(trapBaseString);
+                                trapBaseFloat = Float.parseFloat(
+                                    trapBaseString);
                                 System.out.print("What is the height: ");
                                 trapHeightString = scanner.nextLine();
                                 try {
-                                    trapHeightFloat = Float.parseFloat(trapHeightString);
+                                    trapHeightFloat = Float.parseFloat(
+                                            trapHeightString);
+                                // If the input is not a float display error
                                 } catch (NumberFormatException error) {
-                                    System.out.println("Please enter a valid integer "
-                                        + error.getMessage());
+                                    System.out.print("Please enter a valid ");
+                                    System.out.println("number "
+                                    + error.getMessage());
                                 }
-                                trapArea = ((trapBaseFloat + trapSideFloat) * trapHeightFloat) / 2;
-                                System.out.println("The area of the Triangle is " + trapArea + " (cm^2)");
+                                // Calculate area of trapezoid
+                                trapArea = ((trapBaseFloat + trapSideFloat)
+                                    * trapHeightFloat) / 2;
+                                // Display the area
+                                System.out.print("The area of the");
+                                System.out.println(" Trapezoid is "
+                                    + trapArea + " (cm^2)");
+                            // If the input is not a float display error
                             } catch (NumberFormatException error) {
-                                System.out.println("Please enter a valid integer "
+                                System.out.print("Please enter a valid ");
+                                System.out.println("number "
                                     + error.getMessage());
                             }
+                        // If the input is not a float display error
                         } catch (NumberFormatException error) {
-                            System.out.println("Please enter a valid integer "
+                            System.out.println("Please enter a valid number "
                                 + error.getMessage());
                         }
-                    } else if(shapeString.equals(SHAPE_THREE)){
+                    } else if (shapeString.equals(SHAPE_THREE)) {
                         System.out.print("What is the side length: ");
-                        trapSideString = scanner.nextLine();
+                        penSideString = scanner.nextLine();
                         try {
-                            trapSideFloat = Float.parseFloat(trapSideString);
-                            System.out.print("What is the base: ");
-                            trapBaseString = scanner.nextLine();
-                            try {
-                                trapBaseFloat = Float.parseFloat(trapBaseString);
-                                System.out.print("What is the height: ");
-                                trapHeightString = scanner.nextLine();
-                                try {
-                                    trapHeightFloat = Float.parseFloat(trapHeightString);
-                                } catch (NumberFormatException error) {
-                                    System.out.println("Please enter a valid integer "
-                                        + error.getMessage());
-                                }
-                                trapArea = ((trapBaseFloat + trapSideFloat) * trapHeightFloat) / 2;
-                                System.out.println("The area of the Triangle is " + trapArea + " (cm^2)");
-                            } catch (NumberFormatException error) {
-                                System.out.println("Please enter a valid integer "
-                                    + error.getMessage());
-                            }
+                            penSideDouble = Double.parseDouble(penSideString);
+                            // Calculate area of pentagon
+                            penArea = ((penSideDouble * penSideDouble)
+                                * (Math.sqrt(5
+                                    * (5 + (2 * (Math.sqrt(5))))))) / 4;
+                            // Display the area
+                            System.out.print("The area of the Pentagon is ");
+                            System.out.format("%.2f", penArea);
+                            System.out.println(" (cm^2)");
+                        // If the input is not a float display error
                         } catch (NumberFormatException error) {
-                            System.out.println("Please enter a valid integer "
+                            System.out.println("Please enter a valid number "
                                 + error.getMessage());
                         }
-                    }else {
-                        System.out.println("Sorry, the input does not work, please try again.");
+                    // If the input is not one of the shapes display error
+                    } else {
+                        System.out.print("Sorry, the input does not work");
+                        System.out.println(" please try again.");
                     }
 
-                } else if (userInt == 2){
+                // Find the average of 3 numbers
+                } else if (userInt == 2) {
                     System.out.print("What is your first num(0-100): ");
                     userNum1Sr = scanner.nextLine();
                     try {
+                        // Convert number to float
                         userNum1Float = Float.parseFloat(userNum1Sr);
                         System.out.print("What is your second num(0-100): ");
                         userNum2Sr = scanner.nextLine();
                         try {
+                            // Convert number to float
                             userNum2Float = Float.parseFloat(userNum2Sr);
                             System.out.print("What is your third num(0-100): ");
                             userNum3Sr = scanner.nextLine();
                             try {
+                                // Convert number to float
                                 userNum3Float = Float.parseFloat(userNum3Sr);
-                                average = (userNum1Float + userNum2Float + userNum3Float)/3;
-                                System.out.println("The average is "+ average);
+                                // Calculate the average
+                                average = (userNum1Float + userNum2Float
+                                    + userNum3Float) / 3;
+                                // display the average
+                                System.out.println("The average is " + average);
+                            // If the input is not a float display error
                             } catch (NumberFormatException error) {
-                                System.out.println("Please enter a valid integer "
+                                System.out.print("Please enter a valid ");
+                                System.out.println("number "
                                     + error.getMessage());
                             }
+                        // If the input is not a float display error
                         } catch (NumberFormatException error) {
-                            System.out.println("Please enter a valid integer "
+                            System.out.println("Please enter a valid number "
                                 + error.getMessage());
                         }
+                    // If the input is not a float display error
                     } catch (NumberFormatException error) {
-                        System.out.println("Please enter a valid integer "
+                        System.out.println("Please enter a valid number "
                             + error.getMessage());
                     }
-                }else {
+                // If the input is not one or two display error
+                } else {
                     System.out.println("Sorry, input is invalid.");
                 }
+            // If the input is not an int display error
             } catch (NumberFormatException error) {
                 System.out.println("Please enter a valid integer "
                     + error.getMessage());
             }
+            // Play again loop
             do {
+                // Ask if they want to play again
                 System.out.println("Would you like to play again?(y/n): ");
-                //Get next line as a string
+                // Get next line as a string
                 quesAns = scanner.nextLine();
-                if (quesAns.equals("n")) {
+                // If n then end program
+                if ("n".equals(quesAns)) {
                     System.out.println("Thank you for playing!!");
+                // If any other letter or num is
+                // used ask them again to enter a valid number
                 } else {
                     System.out.println("Please input a valid string");
                 }
-            } while (!quesAns.equals("y") && !quesAns.equals("n"));
-        } while (quesAns.equals("y"));
+            // Do they while the return is not y or n
+            } while (!"y".equals(quesAns) && !"n".equals(quesAns));
+        // Run the whole program when they say yes to play again.
+        } while ("y".equals(quesAns));
 
-            // Closing Scanner
-            scanner.close();
+        // Closing Scanner
+        scanner.close();
     }
 }
